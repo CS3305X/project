e933f550e84afd21986ab2b626066e23eebc20ad
Conor ODonoghue <c.odonoghue93@gmail.com>
2015-02-11 15:05:40 +0000

    fixed seeds.rb, succesfully creates a test user

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,8 +10,10 @@ Usertype.create(type_id: 1, type_name: "Student")
 Usertype.create(type_id: 2, type_name: "Lecturer")
 Usertype.create(type_id: 3, type_name: "Administrator")
 
-User.create(user_type_id: 1,first_name: "Test",last_name: "User", 
+# Create the intial test user
+User.create(id: 123456789, user_type_id: 1,first_name: "Test",last_name: "User", 
                           email: "123456789@umail.ucc.ie", phone_num: "353871234567", password: "teamprojectx", password_confirmation: "teamprojectx")
 user = User.find_by(email: "123456789@umail.ucc.ie")
-user.id = 123456789
-
+#user.id = nil
+#user.id = 123456789
+#-----------------------------
