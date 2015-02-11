61e86b76371ea9f6bc4ef6699d2a9c8abeb71782
Conor ODonoghue <c.odonoghue93@gmail.com>
2015-02-11 14:55:11 +0000

    changed seeds.rb to create new user in the database on startup

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,3 +10,8 @@ Usertype.create(type_id: 1, type_name: "Student")
 Usertype.create(type_id: 2, type_name: "Lecturer")
 Usertype.create(type_id: 3, type_name: "Administrator")
 
+User.create(user_type_id: 1,first_name: "Test",last_name: "User", 
+                          email: "123456789@umail.ucc.ie", phone_num: "353871234567", password: "teamprojectx", password_confirmation: "teamprojectx")
+user = User.find_by(email: "123456789@umail.ucc.ie")
+user.id = 123456789
+
