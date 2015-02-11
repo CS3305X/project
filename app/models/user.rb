503dde22246ba17f006f14031ef134816007e5e3
Conor ODonoghue <c.odonoghue93@gmail.com>
2015-02-11 10:46:48 +0000

    Basic user model with validations and secure password

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,10 @@ class User < ActiveRecord::Base
   has_many :subjects
   has_many :meetings
   has_many :user_groups
+  # Make sure the User has a valid first and second name.
+  validates :first_name,  presence: true, length: { maximum: 50 }
+  validates :last_name,  presence: true, length: { maximum: 50 }
+  #VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  #alidates :email, presence: true, length: { maximum: 255 },
+  #                  format: { with: VALID_EMAIL_REGEX }
 end
