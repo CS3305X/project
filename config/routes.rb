564fd2749124c1fccaa7573790cd8367bd57eef5
Conor ODonoghue <c.odonoghue93@gmail.com>
2015-02-18 00:50:12 +0000

    Added signup page to the routes.rb file

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,10 @@ Rails.application.routes.draw do
   get    'login'   => 'sessions#new'
   post   'login'   => 'sessions#create'
   delete 'logout'  => 'sessions#destroy'
+  
+  get    'signup'   => 'users#new'
+  post   'signup'   => 'users#create'
+  #delete 'logout'  => 'sessions#destroy'
 
   resources :user_groups
 
