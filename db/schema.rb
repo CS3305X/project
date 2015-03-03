98125f3cfcaad4678bf430ece55849e513f65516
Will Murphy <112456672@umail.ucc.ie>
2015-03-03 09:45:30 +0000

    Added to DB Seeds File

--- db/schema.rb
+++ db/schema.rb
@@ -32,13 +32,6 @@ ActiveRecord::Schema.define(version: 20150302223950) do
     t.string   "title"
   end
 
-  create_table "courses", force: true do |t|
-    t.string   "course_id"
-    t.string   "course_name"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
   create_table "events", force: true do |t|
     t.datetime "created_at",                  null: false
     t.datetime "updated_at",                  null: false
