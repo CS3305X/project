974120fe2444bf0f1626d74972c502e9de304f82
Conor ODonoghue <c.odonoghue93@gmail.com>
2015-03-04 18:38:25 +0000

    made modules more informative

--- app/helpers/registered_fors_helper.rb
+++ app/helpers/registered_fors_helper.rb
@@ -4,4 +4,23 @@ module RegisteredForsHelper
         credits = user.credits
         credits
     end 
+    
+    def lecturer_name(id)
+        lecturer = User.find_by(id: id)
+        if !lecturer.nil?
+            return "#{lecturer.first_name} #{lecturer.last_name}"
+        else
+           return id 
+        end
+    end
+    
+    def getName(module_code)
+       subject = Subject.find_by(module_code: module_code) 
+       subject.description
+    end
+    
+    def getCredits(module_code)
+        subject = Subject.find_by(module_code: module_code) 
+        subject.credits
+    end 
 end
