8bdebd51686aff41f1ac5e964b44ebcaef70c756
cmandersen <111378696@umail.ucc.ie>
2015-03-04 23:50:06 +0000

    Change to wokring admin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -22,8 +22,8 @@ class ApplicationController < ActionController::Base
   
   
   def admin?
-    true if session[:user_type_id] == "3"
-    #current_user.user_type_id == 3 
+    #true if session[:user_type_id] == "3"
+    current_user.user_type_id == 3 
   end  
   helper_method :admin?
   
