e757dc456aa5e594e654482f03c441a8da8cfa60
Will Murphy <112456672@umail.ucc.ie>
2015-03-05 00:44:26 +0000

    Added to Seeds File

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,27 +1,49 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+#Create User Types
 Usertype.create(type_id: 1, type_name: "Student")
 Usertype.create(type_id: 2, type_name: "Lecturer")
 Usertype.create(type_id: 3, type_name: "Administrator")
 
-# Create the intial test user
+# Create Students
 User.create(id: 123456789, user_type_id: 1,first_name: "Joe",last_name: "Blog", 
                           email: "123456789@umail.ucc.ie", phone_num: "353871234567", password: "teamprojectx", password_confirmation: "teamprojectx", public_profile: true)
-
-# Create Lecturer Account
+User.create(id: 112456672, user_type_id: 1,first_name: "Will",last_name: "Murphy", 
+                          email: "112456672@umail.ucc.ie", phone_num: "353871234567", password: "teamprojectx", password_confirmation: "teamprojectx", public_profile: true)
+User.create(id: 111378696 , user_type_id: 1,first_name: "Chris",last_name: "Andersen", 
+                          email: "111378696 @umail.ucc.ie", phone_num: "353871234567", password: "teamprojectx", password_confirmation: "teamprojectx", public_profile: true)
+User.create(id: 112501527, user_type_id: 1,first_name: "Olivia",last_name: "Murphy", 
+                          email: "112501527@umail.ucc.ie", phone_num: "353871234567", password: "teamprojectx", password_confirmation: "teamprojectx", public_profile: true)
+User.create(id: 111385351 , user_type_id: 1,first_name: "Conor",last_name: "O'Donoghue", 
+                          email: "111385351 @umail.ucc.ie", phone_num: "353871234567", password: "teamprojectx", password_confirmation: "teamprojectx", public_profile: true)
+User.create(id: 112550207 , user_type_id: 1,first_name: "Anthony",last_name: "O Flynn", 
+                          email: "112550207@umail.ucc.ie", phone_num: "353871234567", password: "teamprojectx", password_confirmation: "teamprojectx", public_profile: true)
+                          
+# Create Lecturers
 User.create(id: 11, user_type_id: 2,first_name: "James",last_name: "Doherty", 
-                          email: "11@umail.ucc.ie", phone_num: "353871234567", password: "teamprojectx", password_confirmation: "teamprojectx", public_profile: true)
+                          email: "jd@umail.ucc.ie", phone_num: "353871234567", password: "teamprojectx", password_confirmation: "teamprojectx", public_profile: true)
+User.create(id: 12, user_type_id: 2,first_name: "Derek",last_name: "Bridge", 
+                          email: "db@umail.ucc.ie", phone_num: "353871234567", password: "teamprojectx", password_confirmation: "teamprojectx", public_profile: true)
+User.create(id: 13, user_type_id: 2,first_name: "Frank",last_name: "Boehme", 
+                          email: "fb@umail.ucc.ie", phone_num: "353871234567", password: "teamprojectx", password_confirmation: "teamprojectx", public_profile: true)
+User.create(id: 14, user_type_id: 2,first_name: "Marc",last_name: "Van Dongen", 
+                          email: "mvd@umail.ucc.ie", phone_num: "353871234567", password: "teamprojectx", password_confirmation: "teamprojectx", public_profile: true)
+User.create(id: 15, user_type_id: 2,first_name: "John",last_name: "Vaughan", 
+                          email: "jv@umail.ucc.ie", phone_num: "353871234567", password: "teamprojectx", password_confirmation: "teamprojectx", public_profile: true)
+User.create(id: 16, user_type_id: 2,first_name: "Kieran",last_name: "Herley", 
+                          email: "kh@umail.ucc.ie", phone_num: "353871234567", password: "teamprojectx", password_confirmation: "teamprojectx", public_profile: true)
+User.create(id: 17, user_type_id: 2,first_name: "Ken",last_name: "Browne", 
+                          email: "kb@umail.ucc.ie", phone_num: "353871234567", password: "teamprojectx", password_confirmation: "teamprojectx", public_profile: true)
+User.create(id: 18, user_type_id: 2,first_name: "Michel",last_name: "Schellekens", 
+                          email: "ms@umail.ucc.ie", phone_num: "353871234567", password: "teamprojectx", password_confirmation: "teamprojectx", public_profile: true)     
                           
 # Create Admin Account
 User.create(id: 001, user_type_id: 3,first_name: "Admin",last_name: "Account", 
                           email: "admin@umail.ucc.ie", phone_num: "353871234567", password: "teamprojectx", password_confirmation: "teamprojectx")
                           
 
+#Create First and Second Year CS Modules
 Subject.create(module_code: "CS1101", description: "System Organisation 1", lecturer_id:11, credits: 5)
 Subject.create(module_code: "CS1111", description: "System Organisation 2", lecturer_id:11, credits: 5)
 Subject.create(module_code: "CS1112", description: "Foundations of Computer Science 1", lecturer_id:13, credits: 5)
@@ -29,7 +51,6 @@ Subject.create(module_code: "CS1113", description: "Foundations of Computer Scie
 Subject.create(module_code: "CS1114", description: "Web Development 1", lecturer_id:15, credits: 5)
 Subject.create(module_code: "CS1115", description: "Web Development 2", lecturer_id:16, credits: 5)
 Subject.create(module_code: "CS1118", description: "Multimedia", lecturer_id:17,credits: 5)
-
 Subject.create(module_code: "CS2500", description: "Software Development", lecturer_id:21, credits: 15)
 Subject.create(module_code: "CS2501", description: "Database Design and Development", lecturer_id:22,credits: 5)
 Subject.create(module_code: "CS2502", description: "Logic Design", lecturer_id:23, credits: 5)
@@ -42,18 +63,4 @@ Subject.create(module_code: "CS2509", description: "Computer Animation", lecture
 Subject.create(module_code: "CS2509", description: "XML and the Extended Enterprise", lecturer_id:30, credits: 5)
 Subject.create(module_code: "CS2510", description: "Web Servers", lecturer_id:31, credits: 5)
 Subject.create(module_code: "CS2511", description: "Usability Engineering", lecturer_id:32, credits: 5)
-
-
-#Create Third Year Subjects
-
-#Class_Schedule.create( module_code:"CS1101", start_time:"2015-26-02 13:00:00", end_time:"2015-26-02 14:00:00")
-#Class_Schedule.create( module_code:"CS1111", start_time:"2015-26-02 12:00:00", end_time:"2015-26-02 13:00:00")
-#Class_Schedule.create( module_code:"CS1112", start_time:"2015-26-02 14:00:00", end_time:"2015-26-02 15:00:00")
-#Class_Schedule.create( module_code:"CS1113", start_time:"2015-26-02 15:00:00", end_time:"2015-26-02 16:00:00")
-#Class_Schedule.create( module_code:"CS1114", start_time:"2015-27-02 13:00:00", end_time:"2015-27-02 14:00:00")
-#Class_Schedule.create( module_code:"CS1115", start_time:"2015-26-02 14:00:00", end_time:"2015-26-02 15:00:00")
-
-#Class_Schedule.create( module_code:"CS1118", start_time:"2015-26-02 15:00:00", end_time:"2015-26-02 16:00:00")
-
-#Class_Schedule.create( module_code:"CS1118", start_time:"2015-26-02 15:00:00", end_time:"2015-26-02 16:00:00")
 
